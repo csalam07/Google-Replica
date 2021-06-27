f1abfba8048a8063281ac5c00121060ffd8b9703
csalam07 <alamcs50@gmail.com>
2021-06-27 22:38:16 +0545

    added dummy response

--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/dist/client/router";
 import Head from "next/head";
 import SearchHeader from "../components/SearchHeader";
+import Response from "../response";
 import SearchResults from "../components/SearchResults";
 
 function Search({ results }) {
@@ -22,10 +23,13 @@ function Search({ results }) {
 export default Search;
 
 export async function getServerSideProps(context) {
+  const useDummyData = false;
   const startIndex = context.query.start || "0";
-  const data = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API}&cx=${process.env.GOOGLE_CONTEXT}&q=${context.query.term}&start=${startIndex}`
-  ).then((response) => response.json());
+  const data = useDummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API}&cx=${process.env.GOOGLE_CONTEXT}&q=${context.query.term}&start=${startIndex}`
+      ).then((response) => response.json());
 
   // After the SERVER has been rendered and pass the result to client.
   return {
